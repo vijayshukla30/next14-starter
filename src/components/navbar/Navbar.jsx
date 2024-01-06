8fbb61c2dd0cab86f57be410e6d96a1aee1409a9
Vijay Shukla <vshukla684@gmail.com>
2024-01-06 16:45:06 +0530

    css and module css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Links from "./links/Links";
-
+import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Logo</div>
       <div>
         <Links />
