838d75ccda53e09778a232683dc17018edcdf38e
Vijay Shukla <vshukla684@gmail.com>
2024-01-06 19:53:47 +0530

    design blog list and fix external image link

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./blog.module.css";
+import PostCard from "@/components/postCard/postCard";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+      <div className={styles.post}>
+        <PostCard />
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
