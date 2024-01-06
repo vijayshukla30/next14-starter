3d37d3e9ad57090fd197448fcb434267a68131b9
Vijay Shukla <vshukla684@gmail.com>
2024-01-06 19:27:29 +0530

    design contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const Contact = () => {
-  return <div>Contact Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="Contact" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea cols="30" rows="10" placeholder="Message"></textarea>
+          <button className={styles.btn}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
