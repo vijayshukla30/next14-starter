a1bf329836629c6730da8f09cc1fb26d483a3f70
Vijay Shukla <vshukla684@gmail.com>
2024-01-06 19:08:15 +0530

    design about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,33 @@ import Image from "next/image";
 import styles from "./about.module.css";
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.header}>About Code Dexterous</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.description}>
+          Next.js 14 Complete Course 2024 | Next.js 14 Full Stack App Tutorial
+          for Beginners
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="about" fill />
+        <Image src="/about.png" alt="hero" fill className={styles.img} />
       </div>
     </div>
   );
