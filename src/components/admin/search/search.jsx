0d38919dca48693bab4114997d4fc8f7c7aec2bf
Vijay Shukla <vshukla684@gmail.com>
2024-01-11 11:03:46 +0530

    Add search functionality

--- src/components/admin/search/search.jsx
+++ src/components/admin/search/search.jsx
@@ -1,11 +1,29 @@
+"use client";
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = (e) => {
+    if (!e.target.value) {
+      return replace(`${pathname}`);
+    }
+    const params = new URLSearchParams(searchParams);
+    params.set("q", e.target.value);
+    replace(`${pathname}?${params}`);
+  };
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
